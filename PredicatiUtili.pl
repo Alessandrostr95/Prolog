bcc86cb59fb9a79c315cac156db576c16f14585e
Alessandro <54322448+Alessandrostr95@users.noreply.github.com>
2020-06-08 23:59:35 +0200

    Create PredicatiUtili.pl

---
+++ PredicatiUtili.pl
@@ -0,0 +1,12 @@
+%listaElementiUguali(Elemento, NumVolte, Lista).
+
+%(1) with maplist/2: creates a list of length N and then matches all its elements to X.
+
+listaElementiUguali(X, N, List)  :- 
+    length(List, N), 
+    maplist(=(X), List).
+
+%(2) with findall/3: makes loop N-times and completes List with X N-times
+
+listaElementiUguali(X, N, List)  :- 
+    findall(X, between(1, N, _), List).
