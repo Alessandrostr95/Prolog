01dfc004b1d24d87f0a18cdec91e023753e7e8a2
Alessandro <54322448+Alessandrostr95@users.noreply.github.com>
2020-06-09 12:54:58 +0200

    Update lineaOrizzontaleInUnPiano.pl

--- lineaOrizzontaleInUnPiano.pl
+++ lineaOrizzontaleInUnPiano.pl
@@ -1,7 +1,9 @@
+/* Vero se Lista è una lista di N elementi tutti uguali di tipo Elemento */
 listaElementiUguali(Lista, Elemento, N):- 
     length(Lista, N), 
     maplist(=(Elemento), Lista).
 
+/* Vero se X è una sottosequenza consecutiva di L */
 sequenza(X,L):-
     append(_,Fine,L),
     append(X,_,Fine).
@@ -21,10 +23,12 @@ piano([ [a1,a1,a1,a4,a4],
 piano([ [a1,a1,a1,a4,a4],
         [a15,a16,a16,a16,a9]]).
 
+/* vero se Seq è una sottolista di Lista di tutti elementi uguali e consecutivi */
 sottosequenzaElementiUguali(Seq, Lista):-
     sequenza(Seq, Lista),
     listaElementiUguali(Seq,_,_).
 
+/* vero se TutteSottoseq è una lista di tutte le sottosequenze di elementi uguali e consecutivi di Lista */
 tutteSottosequenzaElementiUguali(Lista, TutteSottoseq):-
     setof(Seq, sottosequenzaElementiUguali(Seq,Lista), TutteSottoseq).
 
@@ -42,6 +46,9 @@ lineaOrizzontaleInUnPianoFattaDi(A,Lunghezza,[R1|AltreRighe]):-
     write(b),
     lineaOrizzontaleInUnPianoFattaDi(A,Lunghezza,AltreRighe).*/
 
+/* vero se:
+    - P è un piano
+    - esiste nel piano P una linea di A elementi lunga Lunghezza*/
 lineaOrizzontaleInUnPianoFattaDi(A,Lunghezza,P):-
     piano(P),
     member(Riga,P),
