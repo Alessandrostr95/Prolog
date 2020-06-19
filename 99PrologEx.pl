bb2f89c4fc81e83ee2f00c333f209896a9eaefc0
Alessandro <54322448+Alessandrostr95@users.noreply.github.com>
2020-06-19 18:23:13 +0200

    aggiunto es 10

--- 99PrologEx.pl
+++ 99PrologEx.pl
@@ -137,4 +137,26 @@ pack(L,[Pack1|R]):-
     pack(Coda,R).
 
 
+/*
+ * P10 (*) Run-length encoding of a list.
+ * 	    Use the result of problem P09 to implement the so-called
+ *	    run-length encoding data compression method.
+ *	    Consecutive duplicates of elements are encoded as terms [N,E]
+ *	    where N is the number of duplicates of the element E.
+ * 
+ * 	    Example:
+ * 	    ?- encode([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+ * 	    X = [[4,a],[1,b],[2,c],[2,a],[1,d][4,e]]
+ * */
+packedListToEconded([],[]).
+packedListToEconded([A|R1],[[N,E]|R2]):-
+    length(A,N),
+    member(E,A),!,
+    packedListToEconded(R1,R2).
+
+encode(L,X):-
+    pack(L,PackedL),
+    packedListToEconded(PackedL,X).
+
+
 
