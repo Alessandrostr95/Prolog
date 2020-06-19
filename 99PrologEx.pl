32122df78a5b1043adb286f054187292489fc396
Alessandro <54322448+Alessandrostr95@users.noreply.github.com>
2020-06-19 19:48:11 +0200

    aggiunta es 11

--- 99PrologEx.pl
+++ 99PrologEx.pl
@@ -159,4 +159,29 @@ encode(L,X):-
     packedListToEconded(PackedL,X).
 
 
+/*
+ * P11 (*) Modified run-length encoding.
+ * 		Modify the result of problem P10 in such a way that if an
+ * 		element has no duplicates it is simply copied into the
+ * 		result list.
+ * 		Only elements with duplicates are transferred as [N,E] terms.
+ * 
+ * 		Example:
+ * 		?- encode_modified([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+ * 		X = [[4,a],b,[2,c],[2,a],d,[4,e]]
+ * */
+packedListToEconded_modified([],[]).
+packedListToEconded_modified([A|R1],[[N,E]|R2]):-
+    length(A,N),
+    N > 1,
+    member(E,A),!,
+    packedListToEconded_modified(R1,R2).
+
+packedListToEconded_modified([[A]|R1],[A|R2]):-
+    packedListToEconded_modified(R1,R2).
+
+encode_modified(L,X):-
+    pack(L,PackedL),
+    packedListToEconded_modified(PackedL,X).
+
 
